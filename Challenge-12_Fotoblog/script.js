db13a92f4d9d6464977b2fa08593256c10f54779
izac_silva <izaclislanio@gmail.com>
2024-03-19 11:35:05 -0300

    Challenge-12, finalizando projeto com animação

--- Challenge-12_Fotoblog/script.js
+++ Challenge-12_Fotoblog/script.js
@@ -1,27 +1,6 @@
-// const chaveAcesso = 'QbfL0xORtS2WRXzDKq_imw6xMpMiKp-o2bPlVchQi80';
-
-// const requisicao = async () => {
-//   const resposta = await fetch('https://api.unsplash.com/photos/random', { // O valor é retornado para a variável response
-//     headers: {
-//       Authorization: `Client-ID ${chaveAcesso}`
-//     }
-//   });
-
-//   const dados = await resposta.json();
-
-//   const imagem = dados.urls.regular;
-//   const titulo = dados.description;
-//   const autor = dados.user.name;
-
-//   document.getElementById('imagem').innerHTML = `<img src="${imagem}" alt="${titulo}">`;
-//   document.getElementById('titulo').textContent = titulo;
-//   document.getElementById('autor').textContent = `Foto por ${autor}`;
-// };
-
-// requisicao();
 
 const numeroImagens = 9;
-const chaveAcesso = 'q54Aism6bAxuXg6tyTaL4PiLWfbGYLvw2s5s35q33w8';
+const chaveAcesso = '';
 
 const requisicao = async () => {
   const imagens = [];
